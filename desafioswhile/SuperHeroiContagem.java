ed3a9dc175778d0dd297d93e61f8c9d181f3b3a1
Saboia-senai <jaozera.senai@gmail.com>
2025-05-07 14:45:11 -0300

    Update SuperHeroiContagem.java

--- desafioswhile/SuperHeroiContagem.java
+++ desafioswhile/SuperHeroiContagem.java
@@ -1,10 +1,40 @@
 package desafioswhile;
 
+import java.util.Scanner;
+
+
 public class SuperHeroiContagem {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Scanner scanner = new Scanner(System.in);
+        final String CODIGO_ARMADILHA = "54";
 
-	}
+        System.out.println("Bem-vindo, Super-Herói!");
+        System.out.print("Digite o número final da contagem para salvar a cidade: ");
+        int numeroFinal = scanner.nextInt();
 
+        System.out.println("\nContagem iniciada...");
+
+        for (int i = 1; i <= numeroFinal; i++) {
+            System.out.println("Contando: " + i);
+
+            if (i % 10 == 0) {
+                System.out.println("\n🚨 ARMADILHA ATIVADA! Digite o código para desativar (dica: 1 a 100)");
+
+                String codigoDigitado;
+                do {
+                    System.out.print("Código da armadilha: ");
+                    codigoDigitado = scanner.next();
+                    if (!codigoDigitado.equals(CODIGO_ARMADILHA)) {
+                        System.out.println("❌ Código incorreto! Tente novamente.");
+                    }
+                } while (!codigoDigitado.equals(CODIGO_ARMADILHA));
+
+                System.out.println("✅ Armadilha desativada! Continuando a missão...\n");
+            }
+        }
+
+        System.out.println("🎉 Parabéns! Você salvou a cidade!");
+        scanner.close();
+    }
 }
